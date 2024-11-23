035296efd09b9d59f5cc2ce675b32088ccc28048
Muhammad-Huzzaifa <huzzaaifa83@gmail.com>
2024-11-23 12:54:20 +0500

    Upload login template

--- Smart_Diet_Planner/Smart_Diet_Planner/settings.py
+++ Smart_Diet_Planner/Smart_Diet_Planner/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Including app to root project
+    'Diet_planner_app.apps.DietPlannerAppConfig'
 ]
 
 MIDDLEWARE = [
