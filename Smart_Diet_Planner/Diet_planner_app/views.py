c3bd1e4a2e1ad74f783836624c5d579ce00ef075
Rudhan <rudhan.lodhi3@gmail.com>
2024-12-06 15:49:11 +0500

    -

--- Smart_Diet_Planner/Diet_planner_app/views.py
+++ Smart_Diet_Planner/Diet_planner_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'Diet_planner_app/Home.html')
+
+def login(request):
+    return render(request, 'Diet_planner_app/Login.html')
--- Smart_Diet_Planner/Smart_Diet_Planner/settings.py
+++ Smart_Diet_Planner/Smart_Diet_Planner/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Diet_planner_app.apps.DietPlannerAppConfig'
+    'Diet_planner_app.apps.DietPlannerAppConfig',
+    'social_django'
 ]
 
 MIDDLEWARE = [
